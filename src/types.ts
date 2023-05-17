23af42b69fe40907d7bc547efea107db841772f9
Peter Derias <peter_derias@hotmail.com>
2023-05-17 11:35:07 +1000

    yeet old scraper

--- src/types.ts
+++ src/types.ts
@@ -57,26 +57,3 @@ export type LocationData = {
     long: number;
   }>;
 }
-
-export type BuildingDatabase = {
-  [buildingId: string]: BuildingData;
-};
-
-export type BuildingData = {
-  name: string;
-  id: string;
-  lat: number;
-  long: number;
-  rooms: {
-    [roomNumber: string]: RoomData;
-  };
-};
-
-export type RoomData = {
-  name: string;
-  id: string;
-  capacity: number;
-  usage: RoomUsage;
-};
-
-export type RoomUsage = "LEC" | "TUT";
