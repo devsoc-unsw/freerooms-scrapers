ac924e63e1184d32f4ad7eaa56a671c9192ac657
Franco Reyes <franco.javier.reyes@gmail.com>
2023-06-25 22:03:11 +1000

    Exclude certain buildings and add locations for new ones

--- src/runScraper.ts
+++ src/runScraper.ts
@@ -5,17 +5,22 @@ import parseBooking from "./parseBooking";
 import scrapeBuildings from "./scrapeBuildings";
 
 const runScrapeJob = async () => {
-
   const buildings = await scrapeBuildings();
-  fs.writeFileSync("./output/buildings.json", JSON.stringify(buildings, null, 2));
-
   const rooms = await scrapeRooms();
-  fs.writeFileSync("./output/rooms.json", JSON.stringify(rooms, null, 2));
+
+  // Filter buildings with no rooms
+  const filteredBuildings = buildings.filter(
+    building => !!rooms.find(room => room.id.startsWith(building.id))
+  );
 
   const bookingPromises = rooms.map(room => scrapeBookings(room.id));
   const bookings = (await Promise.all(bookingPromises)).flat();
   const parsedBookings = bookings.map(parseBooking).flat();
   parsedBookings.sort((a, b) => a.start.getTime() - b.start.getTime());
+
+  // Write to file
+  fs.writeFileSync("./output/buildings.json", JSON.stringify(filteredBuildings, null, 2));
+  fs.writeFileSync("./output/rooms.json", JSON.stringify(rooms, null, 2));
   fs.writeFileSync("./output/bookings.json", JSON.stringify(parsedBookings, null, 2));
 }
 
