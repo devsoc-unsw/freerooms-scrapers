eb91bb76be48a1614a24febc54fec2e25e4ded9b
Franco Reyes <franco.javier.reyes@gmail.com>
2024-03-27 13:41:37 +1100

    Add CI

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const YEAR = process.env.YEAR
   : new Date().getFullYear();
 console.log("Scraping " + YEAR);
 
-export const HASURAGRES_URL = `http://${process.env.HASURAGRES_HOST}:${process.env.HASURAGRES_PORT}`;
+export const HASURAGRES_URL = process.env.HASURAGRES_URL;
 console.log("Url to hit: " + HASURAGRES_URL);
 
 export const HASURAGRES_API_KEY = process.env.HASURAGRES_API_KEY;
