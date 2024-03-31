7dd736c74c0d009db7ce28548af01b82f033abc8
SkellyBG <skellybg245@gmail.com>
2024-03-31 11:57:57 +1100

    doc: style, update README

--- nss/src/scrapeRoomAdditionalInformation.ts
+++ nss/src/scrapeRoomAdditionalInformation.ts
@@ -5,9 +5,10 @@ import { Facilities, FACILITIES_LIST } from "./types";
 export const scrapeRoomFacilities = async (
   id: string
 ): Promise<Facilities> => {
-  const additionalParams: Record<string, any> = {};
-  additionalParams.show = ["show_facilities"];
-  additionalParams.room = id;
+  const additionalParams: Record<string, string> = {
+    show: "show_facilities",
+    room: id
+  };
   const response = await nssFetch("view_rooms", additionalParams);
   const $ = load(response.data);
 
