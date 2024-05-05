7fb83de6d74660925187ec9ac88ad7869568ef2d
Hung <skellybg245@gmail.com>
2024-05-05 22:51:15 +1000

    fix: uppercase floor/seating

--- nss/src/types.ts
+++ nss/src/types.ts
@@ -1,15 +1,14 @@
-
 export type UngroupedRoomBooking = {
   name: string;
   day: string;
   start: string;
   weekPattern: number;
-}
+};
 
 export type RawRoomBooking = UngroupedRoomBooking & {
   roomId: string;
   end: string;
-}
+};
 
 export type RoomBooking = {
   bookingType: string;
@@ -17,7 +16,7 @@ export type RoomBooking = {
   roomId: string;
   start: Date;
   end: Date;
-}
+};
 
 export const FACILITIES_LIST = [
   "Floor/seating",
@@ -26,20 +25,23 @@ export const FACILITIES_LIST = [
   "Audio-visual",
   "Info technology",
   "Writing media",
-  "Services"
+  "Services",
 ] as const;
 
-export type ScrapedFacilities = Record<typeof FACILITIES_LIST[number], string[]>
+export type ScrapedFacilities = Record<
+  (typeof FACILITIES_LIST)[number],
+  string[]
+>;
 
 export enum FacilityFloor {
-  FLAT = "flat",
-  TIERED = "tiered",
-  OTHER = "other"
+  FLAT = "Flat",
+  TIERED = "Tiered",
+  OTHER = "Other",
 }
 
 export enum FacilitySeating {
-  MOVABLE = "movable",
-  FIXED = "fixed"
+  MOVABLE = "Movable",
+  FIXED = "Fixed",
 }
 
 export type MappedFacilities = {
@@ -51,7 +53,7 @@ export type MappedFacilities = {
   infotechnology: string[];
   writingMedia: string[];
   service: string[];
-}
+};
 
 export type Room = {
   abbr: string;
@@ -61,17 +63,17 @@ export type Room = {
   capacity: number;
   school: string;
   buildingId: string;
-}
+};
 
 export type ParsedName = {
   bookingType: string;
   name: string;
-}
+};
 
 export type NameParser = {
   pattern: RegExp;
   parser: (matchGroups: Record<string, string>) => ParsedName;
-}
+};
 
 export type Building = {
   name: string;
@@ -79,8 +81,8 @@ export type Building = {
   lat: number;
   long: number;
   aliases: string[];
-}
+};
 
 export type OverrideData = {
   buildings: Building[];
-}
+};
