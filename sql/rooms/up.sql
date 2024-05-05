80c726256f00c66fab09b5afae8bfc26afb11f09
Hung <skellybg245@gmail.com>
2024-05-05 19:46:20 +1000

    fix: readd comment

--- sql/rooms/up.sql
+++ sql/rooms/up.sql
@@ -9,6 +9,70 @@ CREATE TYPE SeatingTypeEnum AS ENUM (
     'fixed'
 );
 
+-- CREATE TYPE MicrophoneTypeEnum AS ENUM (
+--     'dual radio microphone', 
+--     'lectern (fixed)', 
+--     'neck (navalier)', 
+--     'radio microphone'
+-- );
+
+-- CREATE TYPE AccessibilityTypeEnum AS ENUM (
+--     'hearing loop',
+--     'power at wall',
+--     'power at seat',
+--     'ventilation - air conditioning',
+--     'weekend access',
+--     'wheelchair access - student',
+--     'wheelchair access - teaching',
+--     'wheelchair tablet'
+-- );
+
+-- CREATE TYPE AudiovisualTypeEnum AS ENUM (
+--     'dvd player',
+--     'document camera',
+--     'dual slide projectors',
+--     'projector, 16mm',
+--     'slide projector (one or more)',
+--     'television monitor',
+--     'video cassette recorder'
+-- );
+
+-- CREATE TYPE InfotechnologyTypeEnum AS ENUM (
+--     'collaborative learning space (low tech)',
+--     'digital annotation monitor',
+--     'dual video data projectors',
+--     'hybrid teaching space',
+--     'it lectern',
+--     'it laptop connection',
+--     'it laptop connection (vga)',
+--     'interactive learning space (high tech)',
+--     'interactive display monitor',
+--     'lectopia (ilecture) enable podcasting',
+--     'lectopia (ilecture) make publically accessible',
+--     'lecture capture venue',
+--     'lecture capture with screen capture (limited availability)',
+--     'lecture capture with video feed',
+--     'room linking',
+--     'video data projector',
+--     'web camera with microphone'
+-- );
+
+-- CREATE TYPE WritingMediaTypeEnum AS ENUM (
+--     'blackboard',
+--     'blackboard or whiteboard',
+--     'blackboard, wide',
+--     'dual overhead projectors',
+--     'whiteboard'
+-- );
+
+-- CREATE TYPE ServiceTypeEnum AS ENUM (
+--     'break out rooms',
+--     'cats casual space',
+--     'demonstration benches',
+--     'demonstration gas taps and sinks'  
+-- );
+
+
 CREATE TABLE Rooms (
     "id"                 TEXT PRIMARY KEY,
     "name"               TEXT NOT NULL,
