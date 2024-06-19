2fe4b724140a235bf9aef7220528adf955491617
Hung Pham <skellybg245@gmail.com>
2024-06-19 13:55:23 +1000

    fix: revert facilities PR to unblock Freerooms (#53)

--- sql/rooms/up.sql
+++ sql/rooms/up.sql
@@ -1,29 +1,10 @@
-CREATE TYPE FloorTypeEnum AS ENUM (
-    'Flat', 
-    'Tiered', 
-    'Other'
-);
-
-CREATE TYPE SeatingTypeEnum AS ENUM (
-    'Movable', 
-    'Fixed'
-);
-
 CREATE TABLE Rooms (
-    "id"                 TEXT PRIMARY KEY,
-    "name"               TEXT NOT NULL,
-    "abbr"               TEXT NOT NULL,
-    "usage"              TEXT NOT NULL,
-    "capacity"           INTEGER NOT NULL,
-    "school"             TEXT NOT NULL,
-    "buildingId"         TEXT NOT NULL,
-    "floor"              FloorTypeEnum,
-    "seating"            SeatingTypeEnum,
-    "microphone"         TEXT[] NOT NULL,
-    "accessibility"      TEXT[] NOT NULL,
-    "audiovisual"        TEXT[] NOT NULL,
-    "infotechnology"     TEXT[] NOT NULL,
-    "writingMedia"       TEXT[] NOT NULL,
-    "service"            TEXT[] NOT NULL,
+    "id"            TEXT PRIMARY KEY,
+    "name"          TEXT NOT NULL,
+    "abbr"          TEXT NOT NULL,
+    "usage"         TEXT NOT NULL,
+    "capacity"      INTEGER NOT NULL,
+    "school"        TEXT NOT NULL,
+    "buildingId"    TEXT NOT NULL,
     FOREIGN KEY ("buildingId") REFERENCES Buildings("id") ON DELETE CASCADE
 );
