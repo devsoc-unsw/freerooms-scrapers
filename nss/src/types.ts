bcaed0c7d2b296fef2809ccbca626372f02bd7d3
Michael Tanto <80404890+mt-fns@users.noreply.github.com>
2025-07-14 16:47:41 +1000

    Revert "Add room coordinates to Hasuragres"

--- nss/src/types.ts
+++ nss/src/types.ts
@@ -64,8 +64,6 @@ export type Room = {
   capacity: number;
   school: string;
   buildingId: string;
-  lat: number;
-  long: number;
 };
 
 export type ParsedName = {
@@ -89,9 +87,3 @@ export type Building = {
 export type OverrideData = {
   buildings: Building[];
 };
-
-export type RoomMarkers = {
-  id: string;
-  lat: number;
-  long: number;
-}
