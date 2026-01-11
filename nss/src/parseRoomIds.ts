79c4992b4c331cd6453576c12a4326f08935a722
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2026-01-11 15:06:54 +1100

    chore: fix up regex to handle any case for the room

--- nss/src/parseRoomIds.ts
+++ nss/src/parseRoomIds.ts
@@ -1,5 +1,7 @@
-const REGEX_ROOM_IDS = /(K-[A-Z][0-9]{2}-[0-9]{3})/g
+const REGEX_ROOM_IDS = /K-[A-Z][0-9]{2}-[^ ]*/g
 
 const parseRoomIds = (allocated_location_name: string): string[] => {
     return Array.from(allocated_location_name.matchAll(REGEX_ROOM_IDS)).map((match) => match[0])
 }
+
+export default parseRoomIds;
