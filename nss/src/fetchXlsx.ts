ae2c651f1694bd86bafcec944fe95fc79ade0a5a
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2026-01-13 15:42:01 +1100

    fix: fixed stalling when lots of bookings loaded

--- nss/src/fetchXlsx.ts
+++ nss/src/fetchXlsx.ts
@@ -2,6 +2,7 @@ import { chromium } from "playwright";
 
 const fetchXlsx = async (url: string, filename: string) => {
   // Setup page and go to url
+  console.log("Opening browser");
   const browser = await chromium.launch({ headless: false });
   const zoomedBrowser = await browser.newContext({
     viewport: { width: 1920, height: 1080 },
@@ -10,16 +11,26 @@ const fetchXlsx = async (url: string, filename: string) => {
   await page.goto(url);
 
   // Get click excel button
-  const downloadPromise = page.waitForEvent("download");
   await page.waitForSelector(".e-excel-export button");
-  await page.waitForSelector(".e-appointment-details div");
+  try {
+    await page.waitForSelector(".e-appointment div", {
+      state: "attached",
+      timeout: 10000,
+    });
+  } catch (error) {
+    console.log("No bookings shown for", url);
+  }
+  console.log("Waiting for download");
+  const downloadPromise = page.waitForEvent("download");
   await page.click(".e-excel-export button");
   const download = await downloadPromise;
 
   // Save file
   await download.saveAs(`${filename}.xlsx`);
+  console.log(`Download successful! Saving to ${filename}.xlsx`);
 
   // Close browser
+  console.log("Closing browser");
   await zoomedBrowser.close();
   await browser.close();
 };
