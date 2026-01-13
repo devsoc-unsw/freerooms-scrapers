fdc4bb5da830b916602f2caf2adecfd530124343
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2026-01-13 16:22:17 +1100

    refactor: saved bookings to json and refactored to use promises

--- nss/src/scrapeTTRooms.ts
+++ nss/src/scrapeTTRooms.ts
@@ -3,7 +3,7 @@ import { YEAR } from "./config";
 import decodeXlsx from "./decodeXlsx";
 import fetchXlsx from "./fetchXlsx";
 import { parseBookingRow } from "./parseBookingRow";
-import { RoomSessionIdentity } from "./types";
+import { RoomBooking, RoomSessionIdentity } from "./types";
 
 const TT_URL =
   "https://publish.unsw.edu.au/timetables?date=2025-11-10&view=week&timetableTypeSelected=1e042cb1-547d-41d4-ae93-a1f2c3d34538&selections=1e042cb1-547d-41d4-ae93-a1f2c3d34538__";
@@ -12,7 +12,7 @@ const OPTIONS = "&view=week&days=0,1,2,3,4,5,6&timePeriod=all%20day";
 const BATCH_SIZE = 20;
 const TIME_PERIOD = `&datePeriod=${YEAR}&all_weeks=true`;
 
-const scrapeTTRooms = async () => {
+const scrapeTTRooms = async (): Promise<RoomBooking[]> => {
   const sessionIdentitiesRaw = fs.readFileSync(
     "./sessionIdentities.json",
     "utf8"
@@ -20,6 +20,7 @@ const scrapeTTRooms = async () => {
   const sessionIdentities: RoomSessionIdentity[] =
     JSON.parse(sessionIdentitiesRaw);
 
+  const bookings = [];
   // Process batches of 20 rooms
   for (let i = 0; i < sessionIdentities.length; i += BATCH_SIZE) {
     const batch = sessionIdentities.slice(i, i + BATCH_SIZE);
@@ -35,20 +36,32 @@ const scrapeTTRooms = async () => {
     console.log(`Processing batch ${i} to ${i + BATCH_SIZE}...`);
 
     try {
-      setTimeout(async () => {
-        console.log(url);
-        await fetchXlsx(url, `./${i}`);
-        const ttBookings = decodeXlsx(`./${i}.xlsx`); // This returns excel booking row type
-
-        // Call your function here to test and run 'npm run scrape-publish'
-        for (let i = 0; i < ttBookings.length; i++) {
-          parseBookingRow(ttBookings[i]);
-        }
-      }, 1000);
+      bookings.push(getBookings(url, i));
     } catch (error) {
       console.log(`Continuing with next batch...`);
     }
   }
+
+  return Promise.all(bookings)
+    .then((bookings) => bookings.flat())
+    .then((bookings) => {
+      // TODO: Only here for debugging purposes remove when in prod
+      fs.writeFileSync("bookings.json", JSON.stringify(bookings, null, 2));
+      return bookings;
+    });
+};
+
+const getBookings = async (url: string, i: number) => {
+  await fetchXlsx(url, `./${i}`);
+  const ttBookings = decodeXlsx(`./${i}.xlsx`); // This returns excel booking row type
+  const bookings = [];
+
+  // Call your function here to test and run 'npm run scrape-publish'
+  for (let i = 0; i < ttBookings.length; i++) {
+    bookings.push(parseBookingRow(ttBookings[i]));
+  }
+
+  return bookings.flat();
 };
 
 scrapeTTRooms();
