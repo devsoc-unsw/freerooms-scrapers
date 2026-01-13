28c6612018538985697bb892eb2631a88a4920a9
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2026-01-13 15:45:16 +1100

    fix: added options to url so it fetches entire year and covers all days of week and entire day

--- nss/src/scrapeTTRooms.ts
+++ nss/src/scrapeTTRooms.ts
@@ -1,31 +1,37 @@
 import fs from "fs";
-import fetchXlsx from "./fetchXlsx";
+import { YEAR } from "./config";
 import decodeXlsx from "./decodeXlsx";
-import { BookingsExcelRow, RawRoomBooking, UngroupedRoomBooking } from "./types";
-import {parseBookingRow} from "./parseBookingRow";
-
+import fetchXlsx from "./fetchXlsx";
+import { parseBookingRow } from "./parseBookingRow";
+import { RoomSessionIdentity } from "./types";
 
 const TT_URL =
   "https://publish.unsw.edu.au/timetables?date=2025-11-10&view=week&timetableTypeSelected=1e042cb1-547d-41d4-ae93-a1f2c3d34538&selections=1e042cb1-547d-41d4-ae93-a1f2c3d34538__";
-// const TT_URL = 
-//   "https://publish.unsw.edu.au/timetables?date=2025-12-29&view=month&timetableTypeSelected=1e042cb1-547d-41d4-ae93-a1f2c3d34538&selections=1e042cb1-547d-41d4-ae93-a1f2c3d34538__2655dda1-67e4-4411-3d93-bc865256edd5&searchText=clancy";
+// Specifies bookings for Mon - Sun and 06:00 - 23:30
+const OPTIONS = "&view=week&days=0,1,2,3,4,5,6&timePeriod=all%20day";
 const BATCH_SIZE = 20;
+const TIME_PERIOD = `&datePeriod=${YEAR}&all_weeks=true`;
 
 const scrapeTTRooms = async () => {
   const sessionIdentitiesRaw = fs.readFileSync(
     "./sessionIdentities.json",
     "utf8"
   );
-  const sessionIdentities = JSON.parse(sessionIdentitiesRaw);
+  const sessionIdentities: RoomSessionIdentity[] =
+    JSON.parse(sessionIdentitiesRaw);
 
   // Process batches of 20 rooms
-  for (let i = 0; i < 60; i += BATCH_SIZE) {
+  for (let i = 0; i < sessionIdentities.length; i += BATCH_SIZE) {
     const batch = sessionIdentities.slice(i, i + BATCH_SIZE);
     const url =
       TT_URL +
       batch
         .map((s: { id: string; sessionIdentity: string }) => s.sessionIdentity)
-        .join("_");
+        .join("_") +
+      OPTIONS +
+      TIME_PERIOD;
+
+    console.log(url);
     console.log(`Processing batch ${i} to ${i + BATCH_SIZE}...`);
 
     try {
@@ -33,12 +39,11 @@ const scrapeTTRooms = async () => {
         console.log(url);
         await fetchXlsx(url, `./${i}`);
         const ttBookings = decodeXlsx(`./${i}.xlsx`); // This returns excel booking row type
-        
-        // Call your function here to test and run 'npm run scrape-publish' 
+
+        // Call your function here to test and run 'npm run scrape-publish'
         for (let i = 0; i < ttBookings.length; i++) {
           parseBookingRow(ttBookings[i]);
         }
-
       }, 1000);
     } catch (error) {
       console.log(`Continuing with next batch...`);
