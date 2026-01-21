d7474e0a9c26406890d321a7695b892393829039
chris <123799877+xleonx0x@users.noreply.github.com>
2026-01-21 19:47:05 +1100

    refactor: fetching excel rows is now done in-memory using buffers instead of writing to disk

---
+++ nss/src/bookings/fetchXlsx.ts
@@ -0,0 +1,56 @@
+import { chromium } from "playwright";
+import { BOOKING_LOADING_TIMEOUT } from "../config";
+import decodeXlsx from "./decodeXlsx";
+import { data } from "cheerio/lib/api/attributes";
+
+const fetchXlsx = async (url: string): Promise<Buffer> => {
+  // Setup page and go to url
+  console.log("Opening browser");
+  const browser = await chromium.launch({ headless: true });
+  const zoomedBrowser = await browser.newContext({
+    viewport: { width: 1920, height: 1080 },
+  });
+  const page = await zoomedBrowser.newPage();
+  await page.goto(url);
+
+  // Get click excel button
+  await page.waitForSelector(".e-excel-export button");
+  try {
+    await page.waitForSelector(".e-appointment div", {
+      state: "attached",
+      timeout: BOOKING_LOADING_TIMEOUT,
+    });
+  } catch (error) {
+    // Note: If bookings are supposed to show up, tweak loading timeout
+    console.log("No bookings shown for", url);
+    console.log(
+      "Note: Very low chance this should be happening, please double check theres no bookings via the link. If there is, tweak booking loading timeout"
+    );
+    console.log("Closing browser");
+    await zoomedBrowser.close();
+    await browser.close();
+
+    return Promise.reject();
+  }
+  console.log("Waiting for download");
+  const downloadPromise = page.waitForEvent("download");
+  await page.click(".e-excel-export button");
+  const download = await downloadPromise;
+
+  // Save data into a buffer
+  const stream = await download.createReadStream();
+  let buffers = [];
+  for await (const data of stream) {
+    buffers.push(data);
+  }
+  const buffer = Buffer.concat(buffers);
+
+  // Close browser
+  console.log("Closing browser");
+  await zoomedBrowser.close();
+  await browser.close();
+
+  return buffer;
+};
+
+export default fetchXlsx;
