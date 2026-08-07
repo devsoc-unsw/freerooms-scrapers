709c4941897f4e028c9755db7e0f4fddbce53a17
Taku Kodama <79110363+risu729@users.noreply.github.com>
2026-08-08 00:01:13 +0900

    Remove the LibCal scraper (#91)

--- libcal/sql/bookings/before.sql
+++
@@ -1,8 +0,0 @@
--- Remove all bookings starting in or after the current timeslot
-DELETE FROM Bookings
-WHERE "bookingType" = 'LIB' AND "start" >= date_bin('30 minutes', now(), TIMESTAMPTZ '2001-01-01');
-
--- Truncate all bookings that go through the current timeslot
-UPDATE Bookings
-SET "end" = date_bin('30 minutes', now(), TIMESTAMPTZ '2001-01-01')
-WHERE "bookingType" = 'LIB' AND "end" > date_bin('30 minutes', now(), TIMESTAMPTZ '2001-01-01');
--- libcal/sql/bookings/up.sql
+++
@@ -1,1 +0,0 @@
-../../../sql/bookings/up.sql
